5e2c616cc2311169e0d5c280a75e76d98b4a963d
kay park <kspark@nepes.co.kr>
2018-11-20 08:16:05 +0900

    git ignore add

---
+++ EarlyFireDetection/EarlyFireDetection/.svn/pristine/8c/8cacd82a30dab4751752fa94da2c50d9a33ee94b.svn-base
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection
+# Build directory: /Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_bitwise_op "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/testBin/test_bitwise_op")
+set_tests_properties(test_bitwise_op PROPERTIES  WORKING_DIRECTORY "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build")
+add_test(test_data_driven "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/testBin/test_data_driven")
+set_tests_properties(test_data_driven PROPERTIES  WORKING_DIRECTORY "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build")
---
+++ EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/src/colorModel.cpp" "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/src/colorModel.cpp.o"
+  "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/src/ds.cpp" "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/src/ds.cpp.o"
+  "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/src/fireBehaviorAnalysis.cpp" "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/src/fireBehaviorAnalysis.cpp.o"
+  "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/src/main.cpp" "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/src/main.cpp.o"
+  "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/src/motionDetection.cpp" "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/src/motionDetection.cpp.o"
+  "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/src/opticalFlowTool.cpp" "/Users/kspark/Documents/GitHub/EarlyFireDetection/EarlyFireDetection/EarlyFireDetection/build/CMakeFiles/EarlyFireDetection.dir/src/opticalFlowTool.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
